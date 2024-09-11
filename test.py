b4b7532fd949e6be84fbd7a4da7aad4e7ddccf8a
zockerhuhn <87993969+zockerhuhn@users.noreply.github.com>
2024-09-11 12:39:37 +0000

    Started some testing with tkinter

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
+from tkinter import *
+from tkinter import ttk
 from InitTrackerClasses import creature
 
-tracker:list = []
+cre1 = creature(player=True,name="greif1",tag="enemy",initiative="23",hp=0,maxhp="65")
+cre2 = creature(player=True,name="greif2",tag="enemy",initiative="23",hp=0,maxhp="65")
 
+tracker = [cre1, cre2]
+
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
